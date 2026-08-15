fbb78cbf0950a7e9bcc3acbc311c3c4dfc27e71f
jmartinez-beep <jmartinez@distribuidorajoret.com>
2026-08-15 16:27:36 -0600

    Excluir estado del botón de optimización

--- project_state.py
+++ project_state.py
@@ -6,6 +6,7 @@ EPHEMERAL_STATE_KEYS = {
     "project_save_name", "project_pick", "confirm_delete_project", "_loaded_project_notice",
     "main_project_save_name", "main_project_search", "main_project_pick", "main_confirm_delete_project",
     "main_save_project", "main_open_project", "main_delete_project",
+    "run_screening_optimization",
     "_active_project_name", "_autosave_hash", "_autosave_status", "_autosave_error",
     "_autosave_last_at",
 }
